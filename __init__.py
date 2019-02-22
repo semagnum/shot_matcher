99ecb9938529dba23722943bb6590dc9b5242f89
Spencer Magnusson <46832095+semagnum@users.noreply.github.com>
2019-02-22 16:25:30 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Color Matching Analyzer",
     "author": "Spencer Magnusson",
-    "version": (1, 0, 1),
+    "version": (1, 1, 0),
     "blender": (2, 80, 0),
     "description": "Analyzes colors of an image and applies it to the compositing tree.",
     "warning": "",
