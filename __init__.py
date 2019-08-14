02237616a85abb519378d11949bcb1f5e38eb991
Spencer Magnusson <46832095+semagnum@users.noreply.github.com>
2019-08-14 07:40:00 -0600

    Update version

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ semagnum+blendermarket@gmail.com
 bl_info = {
     "name": "Shot Matcher",
     "author": "Spencer Magnusson",
-    "version": (2, 0, 0),
+    "version": (2, 1, 0),
     "blender": (2, 80, 0),
     "description": "Analyzes colors of an image or movie clip and applies it to the compositing tree.",
     "location": "Image Editor > UI > Shot Matcher & Movie Clip Editor > Tools > Shot Matcher",
