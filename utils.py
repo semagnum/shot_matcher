1720c37489923f2bb0d10affb99a28c022281e87
Spencer Magnusson <46832095+semagnum@users.noreply.github.com>
2019-09-02 11:14:12 -0600

    Corrected assignment

--- utils.py
+++ utils.py
@@ -3,22 +3,22 @@ import bpy
 def frame_analyze(context, image, forceOverwrite):          
     pixels = image.pixels[:] # create a copy (tuple) for read-only access
 
-    max_r = 10000.0
-    max_g = 10000.0
-    max_b = 10000.0
-    min_r = 0.0
-    min_g = 0.0
-    min_b = 0.0
+    max_r = 0.0
+    max_g = 0.0
+    max_b = 0.0
+    min_r = 10000.0
+    min_g = 10000.0
+    min_b = 10000.0
 
     i = 0
     n = len(pixels)
 
     while i < n:
-        if RGBtoV(context.scene.max_color[0], context.scene.max_color[1], context.scene.max_color[2]) < RGBtoV(pixels[i], pixels[i + 1], pixels[i + 2]):
+        if RGBtoV(max_r, max_g, max_b) < RGBtoV(pixels[i], pixels[i + 1], pixels[i + 2]):
             max_r = pixels[i]
             max_g = pixels[i + 1]
             max_b = pixels[i + 2]
-        if RGBtoV(context.scene.min_color[0], context.scene.min_color[1], context.scene.min_color[2]) > RGBtoV(pixels[i], pixels[i + 1], pixels[i + 2]):
+        if RGBtoV(min_r, min_g, min_b) > RGBtoV(pixels[i], pixels[i + 1], pixels[i + 2]):
             min_r = pixels[i]
             min_g = pixels[i + 1]
             min_b = pixels[i + 2]
