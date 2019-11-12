af26a2b19f8792eff42dcfc1c6d48e691f5195b7
Spencer Magnusson <46832095+semagnum@users.noreply.github.com>
2019-11-12 09:01:17 -0700

    Declaring alpha threshold variables

--- globalProps.py
+++ globalProps.py
@@ -18,3 +18,6 @@ bpy.types.Scene.cma_end_frame = bpy.props.IntProperty(
 bpy.types.Scene.cma_frame_step = bpy.props.IntProperty(
     default=10,
     min=1)
+
+bpy.types.Scene.sm_use_alpha_threshold = bpy.props.BoolProperty(name="Use Alpha Threshold")
+bpy.types.Scene.sm_alpha_threshold = bpy.props.FloatProperty(name="Alpha Threshold", description="Threshold of an alpha value for having its pixel considered for analysis", default=0.75, min=0.0, max=1.0, step=5, precision=2)
