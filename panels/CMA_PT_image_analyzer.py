876780b985915ae0d934e9384636580a1176fe44
Spencer Magnusson <46832095+semagnum@users.noreply.github.com>
2019-11-12 13:34:29 -0700

    Changed layout

--- panels/CMA_PT_image_analyzer.py
+++ panels/CMA_PT_image_analyzer.py
@@ -19,17 +19,16 @@ class CMA_PT_image_analyzer(bpy.types.Panel):
         layout.label(text="Automatic Calculation")
 
         layout.operator(CMA_OT_image_calculator.bl_idname, text = 'Calculate Colors', icon='SEQ_HISTOGRAM')
-        
-        layout.label(text="Color Picker")
-        layout.operator(CMA_OT_color_picker.bl_idname, text = 'Color Pick', icon='EYEDROPPER')
-        layout.operator(CMA_OT_color_reset.bl_idname, text = 'Reset Colors', icon='IMAGE_ALPHA')
-        
-        layout.label(text="Apply")
-        layout.operator(CMA_OT_add_image_node.bl_idname, text = 'Apply in Compositor', icon='NODETREE')
-        
         layout.prop(context.scene, "sm_use_alpha_threshold")
         if context.scene.sm_use_alpha_threshold:
             layout.prop(context.scene, "sm_alpha_threshold", slider = True)
         
+        layout.label(text="Color Picker")
+        layout.operator(CMA_OT_color_picker.bl_idname, text = 'Color Pick', icon='EYEDROPPER')
+        layout.operator(CMA_OT_color_reset.bl_idname, text = 'Reset Colors', icon='IMAGE_ALPHA')
+
         layout.prop(context.scene, "max_color", text='White Color')
         layout.prop(context.scene, "min_color", text='Black Color')
+        
+        layout.label(text="Apply")
+        layout.operator(CMA_OT_add_image_node.bl_idname, text = 'Apply in Compositor', icon='NODETREE')
